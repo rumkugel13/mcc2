4de9b3ef8e9511abe8e6948138dd42343be8b085
rumkugel13 <rumkugel13@outlook.com>
2024-08-05 22:12:39 +0200

    add basic compiler driver

---
+++ mcc2/CompilerDriver.cs
@@ -0,0 +1,67 @@
+using System.Diagnostics;
+
+namespace mcc2
+{
+    public class CompilerDriver
+    {
+        public string Preprocessor(string file)
+        {
+            string output = $"{file[..^2]}.i";
+            using Process process = new Process();
+            process.StartInfo.FileName = "gcc";
+            process.StartInfo.Arguments = $"-E -P {file} -o {output}";
+            process.Start();
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+            {
+                Console.WriteLine("Error running Preprocessor");
+            }
+            return output;
+        }
+
+        public string Compile(string file, int stages = 4)
+        {
+            string output = $"{file[..^2]}.s";
+
+            if (stages > 0)
+            {
+                //todo: run lexer
+            }
+            if (stages > 1)
+            {
+                //todo: run parser
+            }
+            if (stages > 2)
+            {
+                //todo: run codegen
+            }
+            if (stages > 3)
+            {
+                //todo: run codeemit
+                File.WriteAllLines(output, [
+                "   .globl main",
+                "main:",
+                "movl   $2, %eax",
+                "ret"
+            ]);
+            }
+
+            return output;
+        }
+
+        public string AssembleAndLink(string file)
+        {
+            string output = $"{file[..^2]}";
+            using Process process = new Process();
+            process.StartInfo.FileName = "gcc";
+            process.StartInfo.Arguments = $"{file} -o {output}";
+            process.Start();
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+            {
+                Console.WriteLine("Error running AssembleAndLink");
+            }
+            return output;
+        }
+    }
+}
--- mcc2/Program.cs
+++ mcc2/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace mcc2
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid Arguments");
+                return -1;
+            }
+
+            string file = args[0];
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("Invalid Source File");
+                return -2;
+            }
+
+            if (!file.EndsWith(".c"))
+            {
+                Console.WriteLine("Not a C source File");
+                return -3;
+            }
+
+            CompilerDriver compilerDriver = new();
+            string processed = compilerDriver.Preprocessor(file);
+
+            int stages = 4;
+            bool assemble = true;
+            if (args.Length == 2)
+            {
+                string option = args[1];
+                if (option == "--lex")
+                    stages = 1;
+                else if (option == "--parse")
+                    stages = 2;
+                else if (option == "--codegen")
+                    stages = 3;
+                else if (option == "-S")
+                    assemble = false;
+            }
+
+            string assembly;
+            try
+            {
+                assembly = compilerDriver.Compile(processed, stages);
+            }
+            catch
+            {
+                Console.WriteLine($"Error on stage {stages}");
+                return -3 - stages;
+            }
+            finally
+            {
+                File.Delete(processed);
+            }
+
+            if (stages == 4 && assemble)
+                compilerDriver.AssembleAndLink(assembly);
+            if (assemble)
+                File.Delete(assembly);
+
+            return 0;
+        }
+    }
+}
