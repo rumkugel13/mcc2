9542cdbb2c8b16fd1bcb85f675a521ec2ea7931e
rumkugel13 <rumkugel13@outlook.com>
2024-09-11 14:40:45 +0200

    test execution and print error messages

--- mcc2.Tests/TestChapter01.cs
+++ mcc2.Tests/TestChapter01.cs
@@ -12,8 +12,9 @@ public class TestChapter01
         {
             lexer.Lex(File.ReadAllText(file));
         }
-        catch
+        catch (Exception e)
         {
+            Console.WriteLine(e.Message);
             return false;
         }
         return true;
@@ -22,7 +23,7 @@ public class TestChapter01
     [TestMethod]
     public void TestInvalidLex()
     {
-        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/invalid_lex");
+        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/invalid_lex").Where(a => a.EndsWith(".c"));
 
         foreach (var file in files)
         {
@@ -40,8 +41,9 @@ public class TestChapter01
         {
             tokens = lexer.Lex(File.ReadAllText(file));
         }
-        catch
+        catch (Exception e)
         {
+            Console.WriteLine(e.Message);
             Assert.Fail($"Expected to pass Lexer for {file}");
         }
 
@@ -50,8 +52,9 @@ public class TestChapter01
         {
             parser.Parse(tokens);
         }
-        catch
+        catch (Exception e)
         {
+            Console.WriteLine(e.Message);
             return false;
         }
 
@@ -61,7 +64,7 @@ public class TestChapter01
     [TestMethod]
     public void TestInvalidParse()
     {
-        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/invalid_parse");
+        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/invalid_parse").Where(a => a.EndsWith(".c"));
 
         foreach (var file in files)
         {
@@ -74,7 +77,7 @@ public class TestChapter01
     [TestMethod]
     public void TestCompileValid()
     {
-        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/valid");
+        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/valid").Where(a => a.EndsWith(".c"));
         CompilerDriver.CompilerOptions compilerOptions = new CompilerDriver.CompilerOptions();
         compilerOptions.Optimizations = CompilerDriver.Optimizations.None;
         compilerOptions.PrettyPrint = false;
@@ -99,8 +102,68 @@ public class TestChapter01
         }
     }
 
+    private int GetReturnVal(string exe)
+    {
+        using Process process = new Process();
+        process.StartInfo.FileName = exe;
+        process.Start();
+        process.WaitForExit();
+        return process.ExitCode;
+    }
+
+    private int GetExpectedReturnVal(string file)
+    {
+        using Process process = new Process();
+        process.StartInfo.FileName = "gcc";
+        process.StartInfo.Arguments = $"{file} -o {file}.out";
+        process.Start();
+        process.WaitForExit();
+        Assert.AreEqual(0, process.ExitCode, "Error running gcc");
+
+        var expected = GetReturnVal($"{file}.out");
+        File.Delete($"{file}.out");
+        return expected;
+    }
+
     [TestMethod]
-    public void TestAll()
+    public void TestExecuteValid()
+    {
+        var files = Directory.GetFiles("../../../../writing-a-c-compiler-tests/tests/chapter_1/valid").Where(a => a.EndsWith(".c"));
+        CompilerDriver.CompilerOptions compilerOptions = new CompilerDriver.CompilerOptions();
+        compilerOptions.Optimizations = CompilerDriver.Optimizations.None;
+        compilerOptions.PrettyPrint = false;
+        compilerOptions.Stages = CompilerDriver.Stages.Emitter;
+
+        foreach (var file in files)
+        {
+            var preProcessed = CompilerDriver.Preprocessor(file);
+            compilerOptions.File = preProcessed;
+
+            var assembly = "";
+            try
+            {
+                assembly = CompilerDriver.Compile(compilerOptions);
+            }
+            catch
+            {
+                Assert.Fail($"Expected compilation to pass for {preProcessed}");
+            }
+            finally
+            {
+                File.Delete(preProcessed);
+            }
+            var result = CompilerDriver.AssembleAndLink(assembly, "");
+            File.Delete(assembly);
+
+            var expected = GetExpectedReturnVal(file);
+            var actual = GetReturnVal(result);
+            File.Delete(result);
+            Assert.AreEqual(expected, actual, $"Expected return values to match for {file}");
+        }
+    }
+
+    [TestMethod]
+    public void TestExternal()
     {
         var testRunner = "../../../../writing-a-c-compiler-tests/test_compiler";
         var mcc2 = "../../../../mcc2/bin/Debug/net8.0/mcc2";
